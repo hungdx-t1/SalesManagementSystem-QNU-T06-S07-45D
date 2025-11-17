4508dfe666a998fe45db03f4b7773fda52f77735
An <annguyenduc369@gmail.com>
2025-11-17 12:23:09 +0700

    Resolve conflict in app.routes.ts and add Supplier route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { OrderPos } from './page/order-pos/order-pos';
+import { SupplierComponent } from './page/supplier/supplier';
+import { Login } from './login/login';
+import { MainLayout } from './shared/layout/main-layout/main-layout';
+import { AuthLayout } from './shared/layout/auth-layout/auth-layout';
+import { RegisterComponent } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: 'order-pos', component: OrderPos },
+      { path: 'supplier', component: SupplierComponent }
+    ]
+  },
+  {
+    path: '',
+    component: AuthLayout,
+    children: [
+      { path: 'login', component: Login },
+      { path: 'register', component: RegisterComponent }
+    ]
+  },
+  { path: '**', redirectTo: 'login' }
+];
+
